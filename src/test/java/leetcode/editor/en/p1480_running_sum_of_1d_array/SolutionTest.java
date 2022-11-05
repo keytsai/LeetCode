943125e51ad0c5c819d407b3ef2182dece002b22
Ken <keytsai513@gmail.com>
2022-11-05 20:30:57 +0800

    #1 P1480_RunningSumOf1dArray test case 3: given_3_1_2_10_1_then_should_return_3_4_6_16_17 - add

--- src/test/java/leetcode/editor/en/p1480_running_sum_of_1d_array/SolutionTest.java
+++ src/test/java/leetcode/editor/en/p1480_running_sum_of_1d_array/SolutionTest.java
@@ -22,4 +22,16 @@ class SolutionTest {
         assertEquals(expect[0], result[0]);
         assertEquals(expect[1], result[1]);
     }
+
+    @Test
+    void given_3_1_2_10_1_then_should_return_3_4_6_16_17() {
+        var result = new Solution().runningSum(new int[]{3,1,2,10,1});
+
+        int[] expect = {3,4,6,16,17};
+        assertEquals(expect[0], result[0]);
+        assertEquals(expect[1], result[1]);
+        assertEquals(expect[2], result[2]);
+        assertEquals(expect[3], result[3]);
+        assertEquals(expect[4], result[4]);
+    }
 }
