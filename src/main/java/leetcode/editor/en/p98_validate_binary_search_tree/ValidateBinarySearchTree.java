1f68eb8850bed2fe222df4afc8ddfcfd842c78c2
Ken <keytsai513@gmail.com>
2022-12-19 22:02:17 +0800

    #8 P98_ValidateBinarySearchTree - pass test 1

---
+++ src/main/java/leetcode/editor/en/p98_validate_binary_search_tree/ValidateBinarySearchTree.java
@@ -0,0 +1,55 @@
+package leetcode.editor.en.p98_validate_binary_search_tree;
+
+// P98_ValidateBinarySearchTree
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import leetcode.editor.en.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+class Solution {
+
+    List<Integer> list = new ArrayList<>();
+
+    public boolean isValidBST(TreeNode root) {
+        traverse(root);
+        for (int i = 0; i < list.size(); i++) {
+            if (i == list.size() -1) {
+                continue;
+            }
+            if (list.get(i) > list.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left);
+        list.add(root.val);
+        traverse(root.right);
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/test/java/leetcode/editor/en/p98_validate_binary_search_tree/SolutionTest.java
@@ -0,0 +1,25 @@
+package leetcode.editor.en.p98_validate_binary_search_tree;
+
+import leetcode.editor.en.utils.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void given_three_node() {
+        var root = TreeNode.createBinaryTree(2, 1, 3);
+        var result = solution.isValidBST(root);
+        assertThat(result).isTrue();
+    }
+}
