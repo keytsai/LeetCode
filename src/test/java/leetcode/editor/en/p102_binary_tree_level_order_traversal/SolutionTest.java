75d7c62c151112bd686b3a70a12922e2c0f34a9b
Ken <keytsai513@gmail.com>
2022-12-19 20:46:05 +0800

    #7 P102_BinaryTreeLevelOrderTraversal - pass test 3

--- src/test/java/leetcode/editor/en/p102_binary_tree_level_order_traversal/SolutionTest.java
+++ src/test/java/leetcode/editor/en/p102_binary_tree_level_order_traversal/SolutionTest.java
@@ -21,13 +21,19 @@ class SolutionTest {
 
     @Test
     void given_null() {
-        assertThat(solution.levelOrder(null)).isNull();
+        assertThat(solution.levelOrder(null)).isEqualTo(new ArrayList<>());
     }
 
     @Test
     void given_one_node() {
         var root = TreeNode.createBinaryTree(1);
         assertThat(solution.levelOrder(root)).isEqualTo(createLevelOrderList(new Integer[]{1}));
+    }
+
+    @Test
+    void three_list() {
+        var root = TreeNode.createBinaryTree(3, 9, 20, null, null, 15, 7);
+        assertThat(solution.levelOrder(root)).isEqualTo(createLevelOrderList(new Integer[]{3}, new Integer[]{9, 20}, new Integer[]{15, 7}));
     }
 
     private List<List<Integer>> createLevelOrderList(Integer[]... arr) {
